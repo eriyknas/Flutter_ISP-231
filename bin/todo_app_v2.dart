17c5f9cb91c0ffe084c923eab17734e19de91d95
s0m1k111 <spavel1207@gmail.com>
2026-04-08 14:20:40 +0300

    primer

--- bin/todo_app_v2.dart
+++ bin/todo_app_v2.dart
@@ -1,3 +1,16 @@
 void main() {
   print('Hello world!');
+  String name = 'Артем';
+  String? name2 = null;
+  var count = 0;
+  var title = 'hello';
+  var x = 5;
+  // x = 'dsf';
+  final id;
+  id = 5;
+  const app = 'Todo';
+  print('$app $id ${count + 5}');
+  List<String> test = ['язык', 'уроки'];
+  Map<String, dynamic> data = {'key': 13};
+  Set<int> id2 = {1,2,3, 1};
 }
